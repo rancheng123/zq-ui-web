e36ff63cd7e529c57b14e4c81a9ab68247f5e97d
rancheng <rancheng@scrmtech.com>
2022-04-24 14:24:01 +0800

    4

---
+++ src/packages/formPage/src/formPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="ZqFormPageWrap">
+    this is zq-input ZqFormPageWrap
+
+  </div>
+
+
+</template>
+<script>
+export default {
+  name: 'ZqFormPage',
+  inheritAttrs: false,
+}
+</script>
