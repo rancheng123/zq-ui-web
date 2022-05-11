8a0f6c77d0e1712835939e9d9f9d7af4d9ffb03d
rancheng <rancheng@scrmtech.com>
2022-05-11 11:29:51 +0800

    2

--- src/packages/formPage/src/index.vue
+++ src/packages/formPage/src/index.vue
@@ -1,24 +1,26 @@
 <template>
   <div class="formPageBox">
-    <popup :type="'formPage'"
+    <popup
+        v-if="formData"
+        :type="'formPage'"
     ></popup>
   </div>
 
 </template>
 <script>
 import popup from "./popup.vue";
+import request from "./utils/request";
 export default {
   name: "ZqFormPage",
+  data(){
+    return {
+      formData: null
+    }
+  },
   components: {
     popup,
   },
   props: {
-    formData: {
-      type: Object,
-      default(){
-        return {}
-      }
-    },
     apiPrefix: {
       type: String,
       default(){
@@ -37,20 +39,45 @@ export default {
         return {}
       }
     },
-    form_id: {
-      type: Number,
-      default(){
-        return -1
-      }
-    },
-    org_id: {
-      type: String,
-      default(){
-        return ''
-      }
-    }
 
   },
+  methods: {
+    async getDetail() {
+      let self = this;
+      let params = {
+        form_id: this.params.form_id,
+        org_id: this.params.org_id,
+      };
+
+      request({
+        url: this.apiPrefix + "cdp/form-app/details",
+        method: "post",
+        data:params,
+      }).then(({ code, data, msg }) => {
+        if (code == 0) {
+          self.formData = JSON.parse(data.form_content);
+
+          self.formData.advance_attribute = JSON.parse(
+              self.formData.advance_attribute
+          );
+          self.formData.basic_attribute = JSON.parse(
+              self.formData.basic_attribute
+          );
+
+          self.formData.field_content = JSON.parse(
+              self.formData.field_content
+          );
+
+
+        } else {
+          this.$zqMessage.error(msg);
+        }
+      });
+    },
+  },
+  mounted(){
+    this.getDetail()
+  }
 };
 </script>
 <style lang="less">
